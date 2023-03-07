999767cc077758231442588b42e87d780903e193
Brielle-Chenier <brielle_chenier@live.com>
2023-03-07 12:13:37 -0500

    popups added

--- src/App.js
+++ src/App.js
@@ -9,46 +9,71 @@ import { taskData } from "./taskData";
 
 
 function App() {
-  // const taskList = ["task 1", "task 2"];
+  
   const [roleSelected, setRole] = useState ('Residents');
   const [timelineSelected, setTimeline] = useState ("beforeFireTasks");
   const [roleIndex, setRoleIndex] = useState (0);  
   const timeList = ["beforeFireTasks", "duringFireTasks"];
+  const [timelineShown, setTimelineShown] = useState (false);
+  const [taskShown, setTaskShown] = useState (false);
+  const [mapShown, setMapShown] = useState (true);
+  
   const handleRoleClicked = (roleName, roleIndex) => {
     setRole (roleName);
     setRoleIndex (roleIndex);
+    setTimelineShown(true);
+    setMapShown(false);
   }
 
   const handleTimelineClicked = (timeline) => {
     setTimeline (timeline);
+    setTaskShown(true);
+  }
+
+  const handleButtonClicked = () => {
+    setMapShown (true);
+    setTaskShown(false);
+    setTimelineShown(false);
   }
 
   return (
     <div className="App">
       <h1>Wildfire Preparedness</h1>
-    <div>
+    {mapShown && (<div>
         <img src={communityImage} alt = "communitymap" usemap = "#mainmap"/>
         <map name = "mainmap">
              <area shape="circ" coords="150,270,30" onClick = {() => handleRoleClicked('Residents', 0)} />
              <area shape="circ" coords="163,422,30" onClick = {() => handleRoleClicked('Transportation', 1)}/>            
 
         </map>
-    </div>
-    <div name = "timelineSection" />
-      {/* <h3>Role Selected: {roleSelected}</h3> */}
-      <h2>{roleSelected} Timeline</h2>
-      <img src= {beforeFire} class = "timelineGraphic" onClick = {() => handleTimelineClicked('beforeFireTasks')} />
-      <img src={duringFire} class = "timelineGraphic" onClick = {() => handleTimelineClicked('duringFireTasks')}/>
-      <img src={afterFire} class = "timelineGraphic" onClick = {() => handleTimelineClicked('afterFireTasks')}/>
+    </div> )}
+
+
+    {!mapShown &&(<div class = "backButton">
+      <button type = "button" onClick = {() => handleButtonClicked('Residents', 0) }> Back </button>
+    </div>)}
+
+    <div class = "taskSection">
+      {timelineShown && (<div>
+        <h2>{roleSelected} Timeline</h2>
+        <img src= {beforeFire} class = "timelineGraphic" onClick = {() => handleTimelineClicked('beforeFireTasks')} />
+        <img src={duringFire} class = "timelineGraphic" onClick = {() => handleTimelineClicked('duringFireTasks')}/>
+        <img src={afterFire} class = "timelineGraphic" onClick = {() => handleTimelineClicked('afterFireTasks')}/>
     
-    
-    <h2>Tasks: {timelineSelected}</h2>
-    <div class = "taskList">
+        
+      </div>)}
+
+      {taskShown && (<div class = "taskSection">
+      <h2>Tasks: {timelineSelected}</h2>
       {/* data: {JSON.stringify(taskData)}; taskData[roleIndex]*/}
       {/* {taskData.map((role, key)=>{return key = {residents} <h1>{role.afterFireTasks}</h1>})} */}
       
-      <h3>{taskData[roleIndex].tasks.afterFireTasks}</h3>
+      <h3 class = "taskSection">{taskData[roleIndex].tasks.afterFireTasks}</h3>
+      </div>)}
+
     </div>
+    
+
 
     </div>
 
