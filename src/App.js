9662b8e8f7c382215f5a80cac1e2c50fb3929591
Keon Senez <ksenez@uwaterloo.ca>
2023-06-20 14:18:33 -0400

    Added Art Acknowledgement

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import DuringFire from './graphics/DuringFire.png';
 import AfterFire from './graphics/AfterFire.png';
 import CommunityImage from './graphics/CommunityWLocations.png';
 import mobilebackground from './graphics/mobilebackground.jpg';
+import tsista from './graphics/tsista.webp';
 
 
 
@@ -36,6 +37,7 @@ function App() {
   const [textShown, setTextShown] = useState(true);
   const [selectedTimeline, setSelectedTimeline] = useState(null);
   const [isDesktop, setDesktop] = useState(false);
+  const [artAcknowledgementShown, setArtAcknowledgementShown] = useState(false);
 
   
   
@@ -66,6 +68,11 @@ useEffect(() => {
     setTimelineShown(true);
     setMapShown(false);
   };
+   // Function to handle when a role is clicked
+   const handleArtClicked = () => {
+    setArtAcknowledgementShown(true);
+    setMapShown(false);
+  };
 
   // Function to handle when a timeline is clicked
   const handleTimelineClicked = (timeline) => {
@@ -81,6 +88,7 @@ useEffect(() => {
     setTaskShown(false);
     setTimelineShown(false);
     setSelectedTimeline(null);
+    setArtAcknowledgementShown(false);
   };
   
   //Function to Create Task lists
@@ -142,10 +150,10 @@ useEffect(() => {
           </button>
         </div>
       )}
-      {/* Main title */}
+      {/* Main title 
       <h1>
         <u className='webpagetitle'>Wildfire Preparedness</u>
-      </h1>
+      </h1>*/}
       {/* Desktop Community map */}
       {mapShown && isDesktop && (
         <div style={{ 
@@ -156,37 +164,39 @@ useEffect(() => {
     }}>
       <img src={CommunityImage} className='map'/>
       {/* Internal Roles map placement*/}
-      <img src={RoleIcon[taskData[10].id + '.png']} roleIndex='10' className='MapImgOpCent' style={{left:'50%',top:'45%',transform: 'translate(-50%, -50%)',}} title={taskData[10].name}onClick={() => handleRoleClicked(taskData[10].name, 10)} tabIndex={0} />
-      <img src={RoleIcon[taskData[3].id + '.png']} className='InternalRoleImg'  style={{left:'33.5%',top:'45%',transform: 'translate(-33.5%, -50%)',}} title={taskData[3].name}onClick={() => handleRoleClicked(taskData[3].name, 3)} tabIndex={0} />
-      <img src={RoleIcon[taskData[12].id + '.png']} className='InternalRoleImg' style={{left:'38%',top:'27%',transform: 'translate(-38%, -32%)',}} title={taskData[12].name}onClick={() => handleRoleClicked(taskData[12].name, 12)} tabIndex={0}/>
-      <img src={RoleIcon[taskData[5].id + '.png']} className='InternalRoleImg' style={{left:'38%',bottom:'37%',transform: 'translate(-38%, 32%)',}} title={taskData[5].name}onClick={() => handleRoleClicked(taskData[5].name, 5)} tabIndex={0}/>
-      <img src={RoleIcon[taskData[0].id + '.png']} className='InternalRoleImg' style={{left:'50%',top:'20%',transform: 'translate(-50%, -25%)',}}title={taskData[0].name}onClick={() => handleRoleClicked(taskData[0].name, 0)} tabIndex={0} />
-      <img src={RoleIcon[taskData[1].id + '.png']} className='InternalRoleImg' style={{left:'50%',bottom:'30%',transform: 'translate(-50%, 25%)',}}title={taskData[1].name}onClick={() => handleRoleClicked(taskData[1].name, 1)} tabIndex={0} />
-      <img src={RoleIcon[taskData[2].id + '.png']} className='InternalRoleImg' style={{right:'38%',top:'27%',transform: 'translate(38%, -32%)',}}title={taskData[2].name}onClick={() => handleRoleClicked(taskData[2].name, 2)} tabIndex={0} />
-      <img src={RoleIcon[taskData[6].id + '.png']} className='InternalRoleImg' style={{right:'38%',bottom:'37%',transform: 'translate(38%, 32%)',}}title={taskData[6].name}onClick={() => handleRoleClicked(taskData[6].name, 6)} tabIndex={0} />
-      <img src={RoleIcon[taskData[4].id + '.png']} className='InternalRoleImg' style={{right:'33.5%',top:'45%',transform: 'translate(33.5%, -50%)',}} title={taskData[4].name}onClick={() => handleRoleClicked(taskData[4].name, 4)} tabIndex={0} />
-      <img src={RoleIcon[taskData[7].id + '.png']} className='MapImgOpRole' style={{left:'50%',top:'35.5%',transform: 'translate(-50%, -40.7%)',}} title={taskData[7].name}onClick={() => handleRoleClicked(taskData[7].name, 7)} tabIndex={0} />
-      <img src={RoleIcon[taskData[8].id + '.png']} className='MapImgOpRole' style={{right:'45%',top:'39.4%',transform: 'translate(45%, -44.4%)',}} title={taskData[8].name}onClick={() => handleRoleClicked(taskData[8].name, 8)} tabIndex={0} />
-      <img src={RoleIcon[taskData[9].id + '.png']} className='MapImgOpRole' style={{left:'45%',top:'39.4%',transform: 'translate(-45%, -44.4%)',}} title={taskData[9].name}onClick={() => handleRoleClicked(taskData[9].name, 9)} tabIndex={0} />
+      <img src={RoleIcon[taskData[10].id + '.png']} roleIndex='10' className='MapImgOpCent' style={{left:'50%',top:'30%',transform: 'translate(-50%, -30%)',}} title={taskData[10].name}onClick={() => handleRoleClicked(taskData[10].name, 10)} tabIndex={0} />
+      <img src={RoleIcon[taskData[3].id + '.png']} className='InternalRoleImg'  style={{left:'33.5%',top:'30%',transform: 'translate(-33.5%, -30%)',}} title={taskData[3].name}onClick={() => handleRoleClicked(taskData[3].name, 3)} tabIndex={0} />
+      <img src={RoleIcon[taskData[12].id + '.png']} className='InternalRoleImg' style={{left:'38%',top:'12%',transform: 'translate(-38%, -12%)',}} title={taskData[12].name}onClick={() => handleRoleClicked(taskData[12].name, 12)} tabIndex={0}/>
+      <img src={RoleIcon[taskData[5].id + '.png']} className='InternalRoleImg' style={{left:'38%',bottom:'52%',transform: 'translate(-38%, 52%)',}} title={taskData[5].name}onClick={() => handleRoleClicked(taskData[5].name, 5)} tabIndex={0}/>
+      <img src={RoleIcon[taskData[0].id + '.png']} className='InternalRoleImg' style={{left:'50%',top:'5%',transform: 'translate(-50%, -5%)',}}title={taskData[0].name}onClick={() => handleRoleClicked(taskData[0].name, 0)} tabIndex={0} />
+      <img src={RoleIcon[taskData[1].id + '.png']} className='InternalRoleImg' style={{left:'50%',bottom:'42%',transform: 'translate(-50%, 42%)',}}title={taskData[1].name}onClick={() => handleRoleClicked(taskData[1].name, 1)} tabIndex={0} />
+      <img src={RoleIcon[taskData[2].id + '.png']} className='InternalRoleImg' style={{right:'38%',top:'12%',transform: 'translate(38%, -12%)',}}title={taskData[2].name}onClick={() => handleRoleClicked(taskData[2].name, 2)} tabIndex={0} />
+      <img src={RoleIcon[taskData[6].id + '.png']} className='InternalRoleImg' style={{right:'38%',bottom:'52%',transform: 'translate(38%, 52%)',}}title={taskData[6].name}onClick={() => handleRoleClicked(taskData[6].name, 6)} tabIndex={0} />
+      <img src={RoleIcon[taskData[4].id + '.png']} className='InternalRoleImg' style={{right:'33.5%',top:'30%',transform: 'translate(33.5%, -30%)',}} title={taskData[4].name}onClick={() => handleRoleClicked(taskData[4].name, 4)} tabIndex={0} />
+      <img src={RoleIcon[taskData[7].id + '.png']} className='MapImgOpRole' style={{left:'50%',top:'25%',transform: 'translate(-50%, -32%)',}} title={taskData[7].name}onClick={() => handleRoleClicked(taskData[7].name, 7)} tabIndex={0} />
+      <img src={RoleIcon[taskData[8].id + '.png']} className='MapImgOpRole' style={{right:'45%',top:'29%',transform: 'translate(45%, -36.4%)',}} title={taskData[8].name}onClick={() => handleRoleClicked(taskData[8].name, 8)} tabIndex={0} />
+      <img src={RoleIcon[taskData[9].id + '.png']} className='MapImgOpRole' style={{left:'45%',top:'29%',transform: 'translate(-45%, -36.4%)',}} title={taskData[9].name}onClick={() => handleRoleClicked(taskData[9].name, 9)} tabIndex={0} />
       {/* Emergency responder map placement*/}
-      <img src={RoleIcon[taskData[13].id + '.png']} className='EmergencyRoleImg'  style={{right:'23%',bottom:'40%',transform: 'translate(23%, 40%)',}} title={taskData[13].name}onClick={() => handleRoleClicked(taskData[13].name, 13)} tabIndex={0} />
-      <img src={RoleIcon[taskData[14].id + '.png']} className='EmergencyRoleImg'  style={{right:'19%',bottom:'27%',transform: 'translate(19%, 27%)',}} title={taskData[14].name}onClick={() => handleRoleClicked(taskData[14].name, 14)} tabIndex={0} />
-      <img src={RoleIcon[taskData[15].id + '.png']} className='EmergencyRoleImg'  style={{right:'31%',bottom:'17%',transform: 'translate(31%, 17%)',scale:'0.8'}} title={taskData[15].name}onClick={() => handleRoleClicked(taskData[15].name, 15)} tabIndex={0} />
-      <img src={RoleIcon[taskData[16].id + '.png']} className='EmergencyRoleImg'  style={{right:'22.5%',bottom:'15.5%',transform: 'translate(22.5%, 15.5%)',scale:'0.8'}} title={taskData[16].name}onClick={() => handleRoleClicked(taskData[16].name, 16)} tabIndex={0} />
-      <img src={RoleIcon[taskData[17].id + '.png']} className='EmergencyRoleImg'  style={{right:'26%',bottom:'26%',transform: 'translate(26%, 26%)',scale:'0.8'}} title={taskData[17].name}onClick={() => handleRoleClicked(taskData[17].name, 17)} tabIndex={0} />
+      <img src={RoleIcon[taskData[13].id + '.png']} className='EmergencyRoleImg'  style={{right:'22.5%',bottom:'53%',transform: 'translate(22.5%, 53%)',scale:'0.9'}} title={taskData[13].name}onClick={() => handleRoleClicked(taskData[13].name, 13)} tabIndex={0} />
+      <img src={RoleIcon[taskData[14].id + '.png']} className='EmergencyRoleImg'  style={{right:'19%',bottom:'42%',transform: 'translate(19%, 42%)',}} title={taskData[14].name}onClick={() => handleRoleClicked(taskData[14].name, 14)} tabIndex={0} />
+      <img src={RoleIcon[taskData[15].id + '.png']} className='EmergencyRoleImg'  style={{right:'31%',bottom:'32%',transform: 'translate(31%, 32%)',scale:'0.8'}} title={taskData[15].name}onClick={() => handleRoleClicked(taskData[15].name, 15)} tabIndex={0} />
+      <img src={RoleIcon[taskData[16].id + '.png']} className='EmergencyRoleImg'  style={{right:'22.5%',bottom:'30.5%',transform: 'translate(22.5%, 30.5%)',scale:'0.8'}} title={taskData[16].name}onClick={() => handleRoleClicked(taskData[16].name, 16)} tabIndex={0} />
+      <img src={RoleIcon[taskData[17].id + '.png']} className='EmergencyRoleImg'  style={{right:'26%',bottom:'41%',transform: 'translate(26%, 41%)',scale:'0.78'}} title={taskData[17].name}onClick={() => handleRoleClicked(taskData[17].name, 17)} tabIndex={0} />
       {/* Evacuation center map placement*/}
-      <img src={RoleIcon[taskData[22].id + '.png']} className='EvacCenterImg'  style={{left:'21%',bottom:'22%',transform: 'translate(-21%, 22%)', scale:'0.9',}} title={taskData[22].name}onClick={() => handleRoleClicked(taskData[22].name, 22)} tabIndex={0} />
-      <img src={RoleIcon[taskData[23].id + '.png']} className='EvacCenterImg'  style={{left:'23.5%',bottom:'38%',transform: 'translate(-23.5%, 38%)',}} title={taskData[23].name}onClick={() => handleRoleClicked(taskData[23].name, 23)} tabIndex={0} />
-      <img src={RoleIcon[taskData[24].id + '.png']} className='EvacCenterImg'  style={{left:'31.5%',bottom:'18%',transform: 'translate(-31.5%, 18%)',scale: '0.85'}} title={taskData[24].name}onClick={() => handleRoleClicked(taskData[24].name, 24)} tabIndex={0} />
-      <img src={RoleIcon[taskData[25].id + '.png']} className='EvacCenterImg'  style={{left:'27%',bottom:'27%',transform: 'translate(-27%, 27%)',}} title={taskData[25].name}onClick={() => handleRoleClicked(taskData[25].name, 25)} tabIndex={0} />
+      <img src={RoleIcon[taskData[18].id + '.png']} className='EvacCenterImg' style={{left:'24.5%',bottom:'30%',transform: 'translate(-24.5%, 30%)', scale: '0.85',}} title={taskData[18].name}onClick={() => handleRoleClicked(taskData[18].name, 18)} tabIndex={0} />
+      <img src={RoleIcon[taskData[22].id + '.png']} className='EvacCenterImg'  style={{left:'20%',bottom:'40%',transform: 'translate(-20%, 40%)', scale:'0.9',}} title={taskData[22].name}onClick={() => handleRoleClicked(taskData[22].name, 22)} tabIndex={0} />
+      <img src={RoleIcon[taskData[23].id + '.png']} className='EvacCenterImg'  style={{left:'23.5%',bottom:'53%',transform: 'translate(-23.5%, 53%)',}} title={taskData[23].name}onClick={() => handleRoleClicked(taskData[23].name, 23)} tabIndex={0} />
+      <img src={RoleIcon[taskData[24].id + '.png']} className='EvacCenterImg'  style={{left:'31.5%',bottom:'33%',transform: 'translate(-31.5%, 33%)',scale: '0.85'}} title={taskData[24].name}onClick={() => handleRoleClicked(taskData[24].name, 24)} tabIndex={0} />
+      <img src={RoleIcon[taskData[25].id + '.png']} className='EvacCenterImg'  style={{left:'27%',bottom:'42%',transform: 'translate(-27%, 42%)',}} title={taskData[25].name}onClick={() => handleRoleClicked(taskData[25].name, 25)} tabIndex={0} />
       {/* External Organizations map placement*/}
-      <img src={RoleIcon[taskData[11].id + '.png']} className='ExternalRoleImg' style={{left:'49.06%',bottom:'17%',transform: 'translate(-49.06%, 19%)',scale:'0.75'}} title={taskData[11].name}onClick={() => handleRoleClicked(taskData[11].name, 11)} tabIndex={0} />
-      <img src={RoleIcon[taskData[18].id + '.png']} className='ExternalRoleImg' style={{left:'41.5%',bottom:'9.25%',transform: 'translate(-41.5%, 9.25%)', scale: '0.7',}} title={taskData[18].name}onClick={() => handleRoleClicked(taskData[18].name, 18)} tabIndex={0} />
-      <img src={RoleIcon[taskData[19].id + '.png']} className='ExternalRoleImg' style={{right:'42%',bottom:'9.5%',transform: 'translate(42%, 9.5%)', scale: '0.9',}} title={taskData[19].name}onClick={() => handleRoleClicked(taskData[19].name, 19)} tabIndex={0} />
-      <img src={RoleIcon[taskData[26].id + '.png']} className='ExternalRoleImg' style={{right:'49.9%',bottom:'7.5%',transform: 'translate(49.9%, 7.5%)',scale:'0.95'}} title={taskData[26].name}onClick={() => handleRoleClicked(taskData[26].name, 26)} tabIndex={0} />
-      <img src={RoleIcon[taskData[21].id + '.png']} className='ExternalRoleImg' style={{left:'40%',bottom:'0.5%',transform: 'translate(-41%, 0.5%)', scale:'0.8'}} title={taskData[21].name}onClick={() => handleRoleClicked(taskData[21].name, 21)} tabIndex={0} />
-      <img src={RoleIcon[taskData[20].id + '.png']} className='ExternalRoleImg' style={{right:'40%',bottom:'0.1%',transform: 'translate(41%, 0.1%)', scale:'0.9'}} title={taskData[20].name}onClick={() => handleRoleClicked(taskData[20].name, 20)} tabIndex={0} />
-      <img src={RoleIcon[taskData[27].id + '.png']} className='ExternalRoleImg' style={{left:'49.25%',bottom:'0.1%',transform: 'translate(-49.25%, 0.1%)', scale:'0.85'}} title={taskData[27].name}onClick={() => handleRoleClicked(taskData[27].name, 27)} tabIndex={0} />    
+      <img src={RoleIcon[taskData[11].id + '.png']} className='ExternalRoleImg' style={{left:'48.5%',bottom:'26%',transform: 'translate(-48.5%, 26%)',scale:'0.7'}} title={taskData[11].name}onClick={() => handleRoleClicked(taskData[11].name, 11)} tabIndex={0} />
+      <img src={RoleIcon[taskData[19].id + '.png']} className='ExternalRoleImg' style={{right:'45%',bottom:'9%',transform: 'translate(45%, 9%)', scale: '0.9',}} title={taskData[19].name}onClick={() => handleRoleClicked(taskData[19].name, 19)} tabIndex={0} />
+      <img src={RoleIcon[taskData[26].id + '.png']} className='ExternalRoleImg' style={{left:'45%',bottom:'9%',transform: 'translate(-45%, 9%)', scale:'0.7'}} title={taskData[26].name}onClick={() => handleRoleClicked(taskData[26].name, 26)} tabIndex={0} />
+      <img src={RoleIcon[taskData[21].id + '.png']} className='ExternalRoleImg' style={{right:'49.25%',bottom:'17%',transform: 'translate(49.25%, 17%)',scale:'0.8'}} title={taskData[21].name}onClick={() => handleRoleClicked(taskData[21].name, 21)} tabIndex={0} />
+      <img src={RoleIcon[taskData[20].id + '.png']} className='ExternalRoleImg' style={{right:'37%',bottom:'9%',transform: 'translate(37%, 9%)', scale:'0.9'}} title={taskData[20].name}onClick={() => handleRoleClicked(taskData[20].name, 20)} tabIndex={0} />
+      <img src={RoleIcon[taskData[27].id + '.png']} className='ExternalRoleImg' style={{left:'37%',bottom:'9%',transform: 'translate(-37%, 9%)', scale:'0.85'}} title={taskData[27].name}onClick={() => handleRoleClicked(taskData[27].name, 27)} tabIndex={0} />
+      {/*Artist Acknowledgement*/}
+      <img src={tsista} className='AckMapImg' style={{left:'5%',top:'5%',transform: 'translate(-5%, -5%)', scale:'0.9'}} title={'Art Acknowledgement'}onClick={() => handleArtClicked()} tabIndex={0} />    
         </div>
       )}
 
@@ -285,6 +295,19 @@ useEffect(() => {
             {timelineSelected === 'After the Fire' && (
               TaskList(taskData[roleIndex].tasks.afterFireTasks)
             )}
+          </div>
+        )}
+        {artAcknowledgementShown && (
+          <div style={{ 
+            width: '100vmax',
+            height: 'auto',
+            alignItems:'center',
+            textAlign:'center', 
+        }}>
+          <a href={'https://hotdogwaterart.com/'} >
+              <img src={tsista} className={"ackGraphic"} />
+              </a>
+            <h2 style={{paddingBottom: '40vh'}}>Illustrations by Hotdog Water Art</h2>
           </div>
         )}
       </div>
